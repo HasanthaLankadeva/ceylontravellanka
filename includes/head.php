1db7bdbecfa3123e5f3b8c74b42772e84a0619ad
HasanthaLankadeva <41467475+HasanthaLankadeva@users.noreply.github.com>
2026-04-29 23:39:36 +0300

    latest updates

--- includes/head.php
+++ includes/head.php
@@ -128,4 +128,3 @@ echo json_encode($schema, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="<?= BASE_URL ?>css/main.min.css">
-<link rel="stylesheet" href="<?= BASE_URL ?>css/style.css">
