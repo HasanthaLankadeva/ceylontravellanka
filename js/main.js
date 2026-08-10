87b3e60a144ac7505ba9e5eaf6568dc915efb6bb
HasanthaLankadeva <41467475+HasanthaLankadeva@users.noreply.github.com>
2026-08-10 17:23:58 +0300

    latest upfates

--- js/main.js
+++ js/main.js
@@ -561,19 +561,24 @@ var _base_path = (_host == 'localhost') ? '/ceylontravellanka/' : '';
             const btn = document.getElementById('submit-btn');
             const successMsg = document.getElementById('success-message');
 
-            const scriptURL = 'https://script.google.com/macros/s/AKfycbxyTi6hDFx8KfbbMymLvloW9bhc26gjORTfupYcDSLsGTyxxzHGm3XAlsr595r8iRxH/exec'; // Replace with your URL
+            //const scriptURL = 'https://script.google.com/macros/s/AKfycbxyTi6hDFx8KfbbMymLvloW9bhc26gjORTfupYcDSLsGTyxxzHGm3XAlsr595r8iRxH/exec'; // Replace with your URL
 
             // UI Feedback: Disable button and show loading state
             btn.disabled = true;
             btn.innerText = 'Sending...';
 
             const formData = new FormData(form);
-
+ 
             fetch(scriptURL, { 
                 method: 'POST', 
                 body: formData
             })
             .then(response => {
+                
+                if($('#campaign').val() == 'EUOne'){
+                    gtag('event', 'conversion', {'send_to': 'AW-18163798890/lgxaCNGd2d4cEOqmltVD'});
+                }
+
                 // Hide the form and show success message
                 form.style.display = 'none';
                 successMsg.style.display = 'block';
